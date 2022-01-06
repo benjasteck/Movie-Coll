9c0db46e569e9d0da4ad3a914f95799ae15c073e
Chantal1208 <chan2292@easv365.dk>
2022-01-06 11:36:49 +0100

    Table columns

--- src/easv/dk/gui/Controller/Controller.java
+++ src/easv/dk/gui/Controller/Controller.java
@@ -1,14 +1,21 @@
 package easv.dk.gui.Controller;
 
+import easv.dk.be.Category;
+import easv.dk.be.Movie;
+import easv.dk.gui.Model.CategoryModel;
+import easv.dk.gui.Model.MovieModel;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 public class Controller {
 
+    @FXML
+    private TableView categoryTable;
+    @FXML
+    private TableView movieTable;
     @FXML
     private Button btnNewCategory;
     @FXML
@@ -71,6 +78,37 @@ public class Controller {
             Stage stage = (Stage) btnCloseApplication.getScene().getWindow();
             stage.close();
         }
+    }
+
+    public void setUpMovieTable(){
+        TableColumn<Movie, String> column1 = new TableColumn<>("Name");
+        column1.setCellValueFactory(new PropertyValueFactory<>("title"));
+        TableColumn<Movie, String> column2 = new TableColumn<>("IMBD Rating");
+        column2.setCellValueFactory(new PropertyValueFactory<>("imbdRating"));
+        TableColumn<Movie, String> column3 = new TableColumn<>("User Rating");
+        column3.setCellValueFactory(new PropertyValueFactory<>("userRating"));
+        TableColumn<Movie, String> column4 = new TableColumn<>("Last Viewed");
+        column4.setCellValueFactory(new PropertyValueFactory<>("lastView"));
+
+        movieTable.getColumns().add(column1);
+        movieTable.getColumns().add(column2);
+        movieTable.getColumns().add(column3);
+        movieTable.getColumns().add(column4);
+
+      //  movieTable.getItems().addAll(MovieModel.getListModel()); -> list in MovieModel needs to be created that it works
+
+    }
+
+    public void setUpCategoryTable(){
+        TableColumn<Category, String> column1 = new TableColumn<>("Name");
+        column1.setCellValueFactory(new PropertyValueFactory<>("name"));
+        TableColumn<Category, String> column2 = new TableColumn<>("Total Movies");
+        column2.setCellValueFactory(new PropertyValueFactory<>("movieCount"));
+
+        categoryTable.getColumns().add(column1);
+        categoryTable.getColumns().add(column2);
+
+       // categoryTable.getItems().addAll(CategoryModel.getListCategory()); -> List in CategoryModel needs to be created first
     }
 
 }
---
+++ src/easv/dk/gui/Model/CategoryModel.java
@@ -0,0 +1,4 @@
+package easv.dk.gui.Model;
+
+public class CategoryModel {
+}
