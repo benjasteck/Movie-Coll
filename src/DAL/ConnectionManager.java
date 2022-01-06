2ecc0a3f30e1ba0f57d213f01b4da82afa685a7c
zahraG1989 <93261667+zahraG1989@users.noreply.github.com>
2022-01-06 11:15:23 +0100

    makingconnectiondb

---
+++ src/DAL/ConnectionManager.java
@@ -0,0 +1,34 @@
+package DAL;
+
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.Properties;
+
+
+
+public class ConnectionManager {
+    private static final String CONFIG_FILE_NAME = "Movie-Coll.cfg";
+    private final SQLServerDataSource ds;
+
+    public ConnectionManager() throws IOException
+    {
+        Properties props = new Properties();
+        props.load(new FileReader(CONFIG_FILE_NAME));
+
+        ds = new SQLServerDataSource();
+        ds.setServerName(props.getProperty("SERVER"));
+        ds.setDatabaseName(props.getProperty("DATABASE"));
+        ds.setPortNumber(Integer.parseInt(props.getProperty("PORT")));
+        ds.setUser(props.getProperty("USER"));
+        ds.setPassword(props.getProperty("PASSWORD"));
+    }
+
+    public Connection getConnection() throws SQLServerException
+    {
+        return ds.getConnection();
+    }
+
+}
